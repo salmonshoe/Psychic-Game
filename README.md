db71bc7e7bf3162d38fad820420e1ab7e617399a
salmonshoe <rafaelreynoso273@gmail.com>
2019-06-09 23:31:11 -0400

    reset changes and improve game code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Psychic-Game
+
+## Brain is Brewin'
+1. Key events to listen for the letters players type
+2. Display game instrctions, wins, and number of times users guess letters correctly. The word chosen from an array must be first shown as underscores.
+3. As users guess the correct letters, reveal them letter-by-letter.
+4. Have letters already guessed displayed in a div and not valid upon another key event
+5. After the user wins/loses the game, they should automatically choose another word and make the user play it.
+
+### Bonuses
+* play a sound or song when the user guesses their word correctly
+* write some stylish CSS to make a design that fits the game theme
+* Save your whole game and it's properties as an object? Organize your game code as an object if possible (except for the key events)
+* save any of the game's functions as methods, and call them underneath the object declarations using event listeners
+* place my global variables and functions above my object
+  * global variables, then objects, then calls
+
+Break the app down into tiny, manageable fragments. This will make the coding process much less frustrating. Learn the ability to solve a large problem by treating it as a set of smaller ones. So split the whole game into many distinct, pseudocoded problems. Focus on the smaller problems, solve it, then move onto another. Always commit work and back it up with Github pushes. Avoid losing hours of my work because I didn't push to GitHub every half hour or so. 
+
+Use the console as a way to debug and troubleshooting problems. A more powerful alternative is to experiment with the [Chrome Devtools Debugger](https://developers.google.com/web/tools/chrome-devtools/)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,42 @@
-//Create an array of words for the game
-var choices = ['pepsi', 'aesthetic', 'capitalism'];
-//Computer chooses a random word from array
-var randomWord = Math.floor(Math.random() * choices.length);
-var vaporWord = choices[randomWord];
-var underScore = [];
-console.log(vaporWord);
-//Create underscores based on length of the word
-var underscoreMaker = () => {
-    for (var i = 0; i < vaporWord.length; i++) {
-        underScore.push("_");
-    }
-    console.log(underScore);
-    return underScore;
+//GLOBAL VARIABLES (this makes every function access your values)
+    //  ====================================
+//1. Create your array of words
+    var wordsList = ['pepsi', 'vaporwave', 'capitalism', 'nineties', 'simpsons'];
+//2. Create your variable for the word computer chooses. Which is the solution.
+    var chosenWord = ""
+//3. Now you need to create a variable that breaks your word into individual letter that can be places in an array.
+    var lettersInChosenWord = [];
+//4. Crate a variable for the blank spaces in the number of letters
+//Why does is have to be blank?
+    var blanks = 0;
+//5. This will be the variable that reveals a mix of blank and solves letters.
+    var blanksAndSuccesses = [];
+//6. A variable for the wrong guesses
+    var wrongGuesses = [];
+
+//7. Lastly, you create the variable that logs in the wins,losses, and letters left
+    var winsCounter = 0;
+    var lossesCounter = 0;
+    var numGuesses = 9; //Nine because that is the limit you set for the game
+
+//FUNCTIONS (This is the code that you will call upon to run when needed.)
+    //  =================
+//1. Create a function to start and restart the game. Define the function first!
+function startGame(){
+    numGuesses = 9; //This will also be used for when you reset the game
+  
+    //Time to make the computer choose a word from your array
+    chosenWord = wordsList[Math.floor(Math.random()*wordsList.length)];
+
+    //Function chooses a word, now it breaks the word to its letters
+    lettersInChosenWord = chosenWord.split();
+
+    //This code will count the number of letters in the word chosen.
+    numBlanks = lettersInChosenWord.length;
+
+    //Print the solution in the console for testing.
+    console.log(chosenWord);
+
+//IMPORTANT! This code will *reset* the guess and success array at each round.
+//IMPORTANT! This code will *reset* the wrong guesses from the previous round.
 }
-
-// Call the function!!! State the name and open/close parenthesis.
-
-//Gets Users guess
-
-document.onkeyup = function (event) {
-    var userPress = event.key;
-    //Check if guess is right
-    if (userPress === underScore) {
-        //Push the right array and tally the letters
-    }
-    //If right, push the right array
-    //If wrong, push to wrong array 
-};
